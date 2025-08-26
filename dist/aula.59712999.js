0f2f3116cfb3b0b5417b1612ed56c32d57b954eb
“Jean” <jeann.hettich@gmail.com>
2025-08-26 10:36:10 -0300

    corrigindo interação indesejada de overflow

--- dist/aula.59712999.js
+++ dist/aula.59712999.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"9JJ5D":[function(require,module,exports,__globalThis) {
+})({"jedq2":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 55633;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["9JJ5D","4M6V8"], "4M6V8", "parcelRequired16c", {})
+},{}]},["jedq2","4M6V8"], "4M6V8", "parcelRequired16c", {})
 
 //# sourceMappingURL=aula.59712999.js.map
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"7dMPt":[function(require,module,exports,__globalThis) {
+})({"gwo3E":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 55633;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -690,6 +690,6 @@ const contaHoras = setInterval(function() {
     }
 }, 1000);
 
-},{}]},["7dMPt","3cYfC"], "3cYfC", "parcelRequired16c", {})
+},{}]},["gwo3E","3cYfC"], "3cYfC", "parcelRequired16c", {})
 
 //# sourceMappingURL=main.js.map
